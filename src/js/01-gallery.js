97a5bdd87bb369d6b8a657126ebd6c58115461f8
lisovenko02 <118771244+lisovenko02@users.noreply.github.com>
2023-05-02 20:12:58 +0300

    done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const markup =  galleryItems.map( ({ preview, original, description }) =>
+`<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+).join("");
 
-console.log(galleryItems);
+gallery.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    overlayOpacity: 0.7,
+  });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+let pauseTime = localStorage.getItem('videoplayer-current-time') ?? 0;
+player.setCurrentTime(Number(pauseTime));
+
+function onUpdTime(data) {
+    localStorage.setItem('videoplayer-current-time',
+    JSON.stringify(data.seconds)
+    );
+    pauseTime = data.seconds;
+}
+
+player.on('timeupdate', throttle(onUpdTime, 1000, {trailing: false}));
